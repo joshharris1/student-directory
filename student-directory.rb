f177efda6118ee2fd9fb177b1d61b192138e713e
Josh Harris <josh.harris212@yahoo.co.uk>
2018-09-27 10:32:38 +0000

    New interactive student-directory

--- student-directory.rb
+++ student-directory.rb
@@ -1,29 +1,46 @@
+student_count = 11
+#first we print the list of students
 students = [
-"Dr Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch Of The West",
-"Terminator",
-"Freddie Krueger",
-"The Joker",
-"Joffery Baratheon",
-"Norman Bates",
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 def print_header
-puts "The students of Villains Academy"
-puts "-------------"
+puts "The student of Villains Academy"
+puts "------------"
 end
-def print(names)
-    names.each do |name|
-        puts name
-    end
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(student)
+  puts "Overall, we have #{student.count} great students"
 end
- print_header
- print(students)
- print_footer(students)
 
+ def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+ end
+
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
